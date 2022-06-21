d5a70b1d9ee009cecb8ed94fe94ee8ed6cb6eca6
Felix Ogbuluijah <felixogbuluijah@yahoo.com>
2022-06-21 12:02:11 +0100

    Connecting app to database

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
-const dbURI = 'mongodb+srv://iromata:pearl123@blog-totu.jkyxy.mongodb.net/blogs-totu?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://iromata:<password>@blog-totu.jkyxy.mongodb.net/blogs-totu?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => console.log('Connected to db'))
+.then((result) => app.listen(3000))
 .catch((err) => console.log(err))
 
 // register view engine
@@ -18,18 +19,54 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 
 // listening for request
-app.listen(3000);
+//app.listen(3000);
 
 app.get('/', (req, res) => {
     //res.send('<p>The home page</p>');
     //res.sendFile('./view/index.html', {root: __dirname})
-    const blogs = [
-        {title: 'kat finds eggs', snippet: 'lorem ipsum dolor sit amet consecteteur'},
-        {title: 'just start it', snippet: 'lorem ipsum dolor sit amet consecteteur'},
-        {title: 'learning nodejs', snippet: 'lorem ipsum dolor sit amet consecteteur'}
-    ]
-    res.render('index', {title: 'Home', blogs});
+    // 
+    
+    res.redirect('/blogs')
 });
+
+
+// getting and saving data in mongoosedb
+// app.get('/add-blog', (req, res) => {
+//     const blog = new Blog({
+//         title: 'new blog 2',
+//         snippet: 'about this new blog',
+//         body: 'more about this new blog'
+//     })
+//     blog.save()
+//     .then((result) => {
+//         res.send(result)
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     })
+// })
+
+// finding all the elements in the collection
+// app.get('/all-blogs', (req, res) => {
+//     Blog.find()
+//     .then((result) => {
+//         res.send(result)
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+// })
+
+// getting a single element from the database
+// app.get('/single-blog', (req, res) => {
+//     Blog.findById('62b0ebac65656dddb5b033a8')
+//     .then((result) => {
+//         res.send(result);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+// })
 
 app.get('/about', (req, res) => {
     //res.send('<p>The about page</p>');
@@ -44,6 +81,16 @@ app.get('/blogs/create', (req, res) => {
 // app.get('/about-us', (req, res) => {
 //     res.redirect('/about');
 // })
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createdAt: -1})
+    .then((result) => {
+        res.render('index', {title: 'All blogs', blogs: result})
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
 
 // 404 page
 app.use((req, res) => {
